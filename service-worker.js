040a2be3069756cab9df38b5e7df9f299129676a
Dima <dimontregyb@email.ua>
2021-01-22 14:03:35 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.f49209ec04cf372157d8dac9576958f1.js"
 );
 
 self.addEventListener('message', (event) => {
